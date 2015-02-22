c189166c627832910fefb8b83cc0a4cc94ba0604
meshdesign <joshdodd@meshfresh.com>
2015-02-22 16:09:24 -0500

    404

--- 404.php
+++ 404.php
@@ -1,14 +1,32 @@
 <?php get_header(); ?>
+<?php get_header('interior'); ?>
+<?php
+ 
+    $imageArray  = get_field('background_image');
+    $imageAlt  = $imageArray['alt'];
+    $imageURL = $imageArray['sizes']['home-bg'];
 
-<div id="content">
-	
-	<h1>Page Not Found</h1>
-	<p>The page you requested could not be found. Perhaps searching will help.</p>
-	
-	<?php get_search_form(); ?>
-
-</div><!-- End of Content -->
+?>
 
 
-<?php get_sidebar(); ?>
+<div class="page-bg-container" style="background-image: url(<?php echo $imageURL; ?>)"></div> 
+
+	<div class="container">
+ 		
+		<br class="clear" />
+
+		<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+		<div class="seven columns offset-by-five ">
+			<div class="page-content">
+				<h1>Page Not Found</h1>
+				<p>The page you requested could not be found. Perhaps searching will help.</p>
+			</div>	
+		</div>
+		<?php endwhile; ?>
+ 
+	</div>
+ 
+ 
+ 
+ 
 <?php get_footer(); ?>
